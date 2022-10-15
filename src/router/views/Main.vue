2ff397f729958fa558aa6393028d277b39bff41d
purpleCode53 <purpleCode53@gmail.com>
2022-10-15 18:52:17 +0900

    add -router, test.md file, change directory

---
+++ src/router/views/Main.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="hello">
+      <h1>router main vue page</h1>
+    </div>
+  </template>
+  
+  <script>
+
+
+
+  export default {
+    name: 'mainPage',
+    props: {
+      msg: String
+    }
+  }
+  </script>
+  
+  <style scoped>
+
+  </style>
+  
